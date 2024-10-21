545f157ed1b6704e6a89f7486e57ead4f33ecba2
Xtypie <xtypie2505@yandex.ru>
2024-10-21 19:14:53 +0200

    add code task 3

--- main.cpp
+++ main.cpp
@@ -54,7 +54,7 @@ void sort(int mas[], int const n)
     int timenum;
     for (int i = 0; i < n; i++)
     {
-        for (int j = i + 1 ; j < n; j++)
+        for (int j = i ; j < n; j++) // была замена (н-1) на н
         {
             if (mas[i] > mas[j])
             {
@@ -87,6 +87,27 @@ void sortsum(int mas[], int massum[], int const n) // сортировка по 
     }
 }
 
+void sortmult(int mas[], int masmult[], int const n) // сортировка по произведению цифр числа
+{
+    int timenum;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0 ; j < n; j++)
+        {
+            if (masmult[i] == multnum(mas[j]))
+            {
+                timenum = mas[i];
+                mas[i] = mas[j];
+                mas[j] = timenum;
+
+            }
+        }
+            
+    }
+}
+
+
+
 void print(int mas[], int const n)
 {
     for (int i = 0; i < n; i++)
@@ -100,22 +121,22 @@ int main()
 {   
     //ЗАДАНИЕ 1
 
-   int n1;
+   /*int n1;
    cin >> n1;
    int* msv = new int[n1];
    for (int i = 0; i < n1; i++) {cin >> msv[i];}
 
    print(msv, n1);
    int simplec = 0;
-   for (int i = 0; i < n1; i++) {if (prost(msv[i])) {simplec++;}}
+   for (int i = 0; i < n1 - 1; i++) {if (prost(msv[i])) {simplec++;}}
 
-    if (simplec == 0) {sort(msv, n1 + 1 );}
+   if (simplec == 0) {sort(msv, n1); print(msv, n1);}
+   else{cout << "simple nums in massive" << endl;} */
 
-    print(msv, n1);
 
     //ЗАДАНИЕ 2
 
-    int n2;
+    /*int n2;
     cin >> n2;
     int* msv2 = new int[n2];
     int* msvsum = new int[n2]; //массив с суммами цифр числа
@@ -124,20 +145,48 @@ int main()
     for (int i = 0; i < n2; i++) {cin >> msv2[i];}
     for (int i = 0; i < n2; i++) {msvsum[i] = sumnum(msv2[i]); msvmult[i] = multnum(msv2[i]);}
     sort(msvsum, n2);
-    sortsum(msv2, msvsum, n2); //сортировка по сумме цифр числа
-    print(msvsum, n2);
-    print(msv2, n2);
-    /*if (povtor(msvsum, n2))
+    sort(msvmult, n2);
+    if (povtor(msvsum, n2))
     {
-
+        sortmult(msv2, msvmult, n2);
     }
-    else
+    else if (!povtor(msvsum, n2))
     {
         sort(msvsum, n2);
-        sortsum(msv, msvsum, n2); //сортировка по сумме цифр числа
-        print(msv, n2);
+        sortsum(msv2, msvsum, n2); //сортировка по сумме цифр числа
     }
-    */
+    else if (povtor(msvmult, n2))
+    {
+        sort(msv2, n2);
+    }
+    print(msv2, n2);*/
+
+    // ЗАДАНИЕ 3
+
+    int row, col, k, mx, str;
+    k = 1;
+    mx, str = 0;
+    cin >> row >> col;
+    int matr[20][20];
+    for (int i = 0; i < row; i++)
+    {
+        if (k > mx){mx = max_element(k, mx); str = i;}
+        k = 1;
+        for (int j = 0; j < col; j++){cin >> matr[i][j]; k *= matr[i][j];}
+
+    }
+    for (int i = 0; i < col; i ++) {matr[str][i] = mx;}
+    for (int i = 0; i < row; i++) 
+    {
+        for (int j = 0; j < col; j++){cout << matr[i][j];}
+        cout << endl;
+    }
+
+    
+
+
+
+    
     
 
 
